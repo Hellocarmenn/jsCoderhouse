332472065d0227d4c168e618e6390c38a27b0aea
carmen <carmenmtz97@comunidad.unam.mx>
2024-04-15 15:05:49 -0600

    Segunda Entrega1

---
+++ codigo.js
@@ -0,0 +1,14 @@
+let opcion = prompt('Ingrese la opcion deseada \n 1.-Cafe 2.-Capuchino 3.Matcha');
+let funcionalidad = prompt('Ingrese la opcion deseada \n 1.-Cafe 2.-Capuchino 3.Matcha');
+
+
+
+while (funcionalidad !=0){
+    switch(funcionalidad){
+
+    }
+}
+
+function filtrarPorTamaño(tamano){
+
+}
---
+++ productos.js
@@ -0,0 +1,44 @@
+const productos =[
+    {
+        id:1,
+        nombre:'Capuchino',
+        precio:80,
+        tamano:'Grande',
+    },
+    {
+        id:2,
+        nombre:'Cafe Americano',
+        precio:65,
+        tamano:'Grande',
+    },
+
+    {
+        id:3,
+        nombre:'Matcha',
+        precio:90,
+        tamano:'Grande',
+    },
+    {
+        id:4,
+        nombre:'Capuchino',
+        precio:40,
+        tamano:'Chico',
+    },
+    {
+        id:5,
+        nombre:'Cafe Americano',
+        precio:35,
+        tamano:'Chico',
+    },
+
+    {
+        id:6,
+        nombre:'Matcha',
+        precio:45,
+        tamano:'Mediano',
+    },
+]
+    
+  
+
+
